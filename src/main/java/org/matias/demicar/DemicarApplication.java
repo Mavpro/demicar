a8151f431fd26288789563e9209034e3502d7d3e
matias <matiasalonso4@gmail.com>
2024-07-09 00:06:16 -0300

    inicio

---
+++ src/main/java/org/matias/demicar/DemicarApplication.java
@@ -0,0 +1,13 @@
+package org.matias.demicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemicarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemicarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/matias/demicar/DemicarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.matias.demicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemicarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
